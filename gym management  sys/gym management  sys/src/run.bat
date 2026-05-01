3c9a71eed791e5612dc1d0e9cac47b4dd1c925b9
Mohd Wasi Imam <wasiimamid@gmail.com>
2026-05-02 01:45:28 +0530

    Delete gym management  sys/gym management  sys directory

--- gym management  sys/gym management  sys/src/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\wasii\Desktop\gym management sys\src"
-javac -cp ".;../lib/ojdbc11.jar" DBConnection.java MemberPanel.java PaymentPanel.java MainFrame.java
-java -cp ".;../lib/ojdbc11.jar" MainFrame
-pause
